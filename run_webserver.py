4365d7ccc35c28f3f1fe339ce4d006b330017f91
peter-murphy-wit <20085832@mail.wit.ie>
2021-03-22 18:45:13 +0000

    dg_id returned to main method

--- run_webserver.py
+++ run_webserver.py
@@ -8,21 +8,22 @@ import sys
 
 ec2 = boto3.resource('ec2')
 ec2_client = boto3.client('ec2')
+s3 = boto3.resource
 
-def create_instance():
+def create_instance(sg_id):
     instance = ec2.create_instances(
         ImageId='ami-079d9017cb651564d',
         MinCount=1,
         MaxCount=1,
         InstanceType='t2.micro',
-        #SecurityGroup='launch-wizard-2'
+        SecurityGroup=sg_id
         )
     print (instance[0].id)
 
 def create_security_group():
     response = ec2_client.create_security_group(
     Description='ACS CA1 Secuirty Group',
-    GroupName='acs-ca1-sg',
+    GroupName='acs-ca1-sg-test-02',
     VpcId='vpc-34c7234d',
     TagSpecifications=[
         {
@@ -37,7 +38,10 @@ def create_security_group():
     ],
     DryRun=False
     )
-    print(response)
+
+    #print(response)
+    sg_id = response['GroupId']
+    return sg_id
 
 def create_bucket():
     for bucket_name in sys.argv[1:]:
@@ -48,9 +52,9 @@ def create_bucket():
         except Exception as error:
             print (error)
 
-def put_bucket():
-    bucket_name = sys.argv[1]
-    object_name = sys.argv[2]
+def put_bucket(b_name, o_name):
+    bucket_name = b_name
+    object_name = o_name
 
     try:
         response = s3.Object(bucket_name,
@@ -60,7 +64,10 @@ def put_bucket():
         print (error)
 
 def main():
-    create_security_group()
+    sg_id = create_security_group()
+    create_instance(sg_id)
+    b_name = create_bucket()
+    put_bucket(b_name, 'bog.jpg')
 
 if __name__ == "__main__":
     main()
